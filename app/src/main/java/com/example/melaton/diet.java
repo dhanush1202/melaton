c03bd336defbc1237f31fa3f2870841159d7bfc6
akshitha <mashettyakshitha@gmail.com>
2022-09-24 03:06:59 +0530

    aks

--- app/src/main/java/com/example/melaton/diet.java
+++ app/src/main/java/com/example/melaton/diet.java
@@ -2,13 +2,23 @@ package com.example.melaton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class diet extends AppCompatActivity {
-
+TextView t;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_diet);
+        t=findViewById(R.id.textView23);
+        t.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(diet.this,weeklyact.class));
+            }
+        });
     }
 }
